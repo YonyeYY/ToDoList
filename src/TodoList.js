e3917f493699b3cb2861db7bf3ad6a29ff2fe373
向永烨 <xiang.yongye@hand-china.com>
2020-03-03 21:49:11 +0800

    3-4 JSX 语法

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,28 +1,80 @@
 import React, { Component, Fragment } from'react';
+import './style.css';
 
 class TodoList extends Component{
 
     constructor(props){
         super(props);
-        
+        this.state = {
+            inputValue: '',
+            list: []
+        }
     }
 
 
     render(){
         return(
-            <Fragment>
+            <Fragment>            
                 <div>
-                    <input/>
-                    <button>提交</button>
+                <label htmlFor="insertArea">输入内容</label>
+                    {/* 下面是一个input框 */}
+                    <input
+                    id="insertArea"
+                    className='input'
+                    value={this.state.inputValue} 
+                    onChange={this.handleInputChange.bind(this)}
+                    />
+                    <button onClick={this.handleBtnClick.bind(this)}>提交</button>
                 </div>
                 <ul>
-                    <li>学英语</li>
-                    <li>Learning React</li>
+                    {
+                        this.state.list.map((item,index)=>{
+                            return (
+                            <li 
+                            key={index} 
+                            onClick={this.handleItemDelete.bind(this,index)}
+                            dangerouslySetInnerHTML={{__html: item}}
+                            >
+                            </li>
+                            )
+                        })
+                    }
                 </ul>
             </Fragment>
         )
     }
+
+    handleInputChange(e){
+        this.setState({
+            inputValue: e.target.value
+        })
+        console.log(e.target);
+    }
+
+    handleBtnClick(e){
+        this.setState({
+            list:[...this.state.list,this.state.inputValue],
+            inputValue:''
+        })
+    }
+
+    handleItemDelete(index){
+        //immutable
+        //state 不允许我们做任何的改变
+        const list = [...this.state.list];
+        list.splice(index,1);
+
+        this.setState({
+            list:list
+        })
+
+        console.log(index);
+    }
+
+
 }
+
+
 
 
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import TodoList from './TodoList';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<TodoList />, document.getElementById('root'));
 
